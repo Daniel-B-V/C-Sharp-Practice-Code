1219945a8677111ee539ae7fd35072fad80a3823
Daniel B. Villanueva <150594473+Daniel-B-V@users.noreply.github.com>
2025-01-21 17:07:44 +0800

    Add files via upload

---
+++ C-Sharp-Codes/GuessGame.cs
@@ -0,0 +1,50 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+    Random random = new Random();
+
+     bool playAgain = true;
+     int max = 101;
+     int min = 1;
+     int guesses;
+     int guess;
+     int number;
+     string answer = "";
+
+    while(playAgain){
+        guesses = 0;
+        guess = 0;
+        number = random.Next(min, max);
+        Console.WriteLine("Guess a number between (1- 100)");
+
+        while(guess != number){
+            Console.Write("Guess: ");
+            guess = Convert.ToInt32(Console.ReadLine());
+
+            if (guess < number){
+                Console.WriteLine("Guess is too low!");
+            }
+            else if (guess > number){
+                Console.WriteLine("Guess is too high!");
+            }
+            guesses++; 
+        }
+        Console.WriteLine("Congrats you guess the right number!");
+        Console.WriteLine("Number of guesses: " + guesses);
+        Console.Write("Do you want to play again? (Y/N): ");
+        answer = Console.ReadLine();
+        answer = answer.ToUpper();
+
+        if (answer == "Y"){
+            playAgain = true;
+        }
+
+        else{
+            playAgain = false;
+        }
+    }
+        Console.WriteLine("Thanks for playing!");
+    }
+}
